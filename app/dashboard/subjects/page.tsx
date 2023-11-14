a61d17d129752ef5092b5d2b0d0833fc777d06a2
euphel <jlcsievon@gmail.com>
2023-11-14 23:12:51 +0800

    idkanymore

--- app/dashboard/subjects/page.tsx
+++ app/dashboard/subjects/page.tsx
@@ -1,7 +1,23 @@
+"use client";
 import React from "react";
+import ProfileBar from "@/components/ProfileBar";
+import { useCollapseContext } from "@/components/provider/collapseProvider";
 
-const page = () => {
-  return <div>page</div>;
+const Subjects = () => {
+  const [collapse, setCollapse] = useCollapseContext();
+
+  return (
+    <div
+      className={`absolute top-0 left-[57px] w-[calc(100vw-57px)] lg:w-[calc(100vw-16rem)] ${
+        collapse ? "lg:left-[57px] w-[calc(100vw-57px)] lg:w-[calc(100vw-57px)]" : "lg:left-[16rem] w-[calc(100vw-16rem)]"
+      } flex -z-0 px-4 py-4 transition-all`}
+    >
+      <ProfileBar />
+      <div className='mt-5'>
+        <div className='min-h-[7.5rem] flex items-center px-4 bg-green-500 text-white text-3xl font-bold rounded-lg '>mathematics</div>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Subjects;

1e6bb75a7afac64a0f0034460bc19c52d66be442
Euphel <jlcsievon@gmail.com>
2023-12-13 16:07:44 +0800

    30% on onboarding

--- app/teacher/onboarding/page.tsx
+++ app/teacher/onboarding/page.tsx
@@ -14,10 +14,8 @@ const OnboardingPage = async () => {
     if (user.first_name == "") {
       return (
         <div className='w-screen h-screen bg-green-500 flex flex-col items-center justify-center'>
-          <div className='bg-white px-6 py-8 rounded-xl min-h-[40rem] flex flex-col gap-6'>
-            <div>
-              <Progress value={50} className='w-96' />
-            </div>
+          <div className='bg-white px-6 py-8 rounded-xl min-h-[40rem] w-96 flex flex-col gap-6'>
+            <div>{/* <Progress value={50} className='w-96' /> */}</div>
             <div>
               <div className='font-semibold text-3xl'>Basic Info</div>
               <div className='text-md text-gray-500'>Tell us a bit more about yourself</div>
--- components/OnboardingForm.tsx
+++ components/OnboardingForm.tsx
@@ -5,6 +5,12 @@ import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
 import { Button } from "./ui/button";
 import { z } from "zod";
+import { CalendarIcon, Link } from "lucide-react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { cn } from "@/lib/utils";
+import { Calendar } from "./ui/calendar";
+import format from "date-fns/format";
 
 const formSchema = z.object({
   first_name: z.string({ required_error: "This field is required" }),
@@ -14,7 +20,7 @@ export const OnboardingForm = () => {
   const form = useForm();
   return (
     <Form {...form}>
-      <form onSubmit={() => {}} className='space-y-8 mt-5'>
+      <form onSubmit={() => {}} className='space-y-4 mt-5'>
         <FormField
           control={form.control}
           name='first_name'
@@ -22,9 +28,79 @@ export const OnboardingForm = () => {
             <FormItem>
               <FormLabel>First Name</FormLabel>
               <FormControl>
-                <Input placeholder='shadcn' {...field} />
+                <Input placeholder='Juan' {...field} />
               </FormControl>
-              <FormDescription>This is your public display name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='middle_name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Middle Name</FormLabel>
+              <FormControl>
+                <Input placeholder='Malvar' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='last_name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Last Name</FormLabel>
+              <FormControl>
+                <Input placeholder='dela Cruz' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='gender'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Gender</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder='Select gender' />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value='Male'>Male</SelectItem>
+                  <SelectItem value='Female'>Female</SelectItem>
+                  <SelectItem value='Other'>Other</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='dob'
+          render={({ field }) => (
+            <FormItem className='flex flex-col'>
+              <FormLabel>Date of birth</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button variant={"outline"} className={cn("w-[240px] pl-3 text-left font-normal", !field.value && "text-muted-foreground")}>
+                      {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                      <CalendarIcon className='ml-auto h-4 w-4 opacity-50' />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className='w-auto p-0' align='start'>
+                  <Calendar mode='single' selected={field.value} onSelect={field.onChange} disabled={(date) => date > new Date() || date < new Date("1900-01-01")} initialFocus />
+                </PopoverContent>
+              </Popover>
               <FormMessage />
             </FormItem>
           )}
